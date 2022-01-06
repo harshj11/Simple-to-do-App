aafb37cc5b8b1cc1fd368685f8e37cef812608d2
Harsh Jain <harshjain5454@gmail.com>
2022-01-06 21:51:41 +0530

    REFRACTOR::hammer:create day module

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const day = require(__dirname + "/util/day");
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 mongoose.connect("mongodb://localhost:27017/simpleTodoDB");
 
@@ -17,86 +23,44 @@ const todosSchema = new mongoose.Schema({
 
 const Todo = mongoose.model("todo", todosSchema);
 
-let currentDay = new Date();
-let dateOnly = currentDay.toISOString().slice(0, 10);
+let selectedDate = new Date();
+let dateOnly = selectedDate.toISOString().slice(0, 10);
 let todos = [];
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public"));
-
 app.get("/", (req, res) => {
-	
-	let salutationText = null, emoji = null;
-	let hours = new Date().getHours();
-	let day = null;
-
-	switch (currentDay.getDay()) {
-		case 0: day = "Sunday";
-			break;
-		case 1: day = "Monday";
-			break;
-		case 2: day = "Tuesday";
-			break;
-		case 3: day = "Wednesday";
-			break;
-		case 4: day = "Thursday";
-			break;
-		case 5: day = "Friday";
-			break;
-		default: day = "Saturday";
-	}
-
-	if(hours >= 4 && hours <= 11) {
-		salutationText = "Good Morning";
-		emoji = "🌄";
-	} else if(hours >= 12 && hours <= 17) {
-		salutationText = "Good Afternoon";
-		emoji = "☀️";
-	} else if(hours >= 18 && hours <= 23) {
-		salutationText = "Good Evening";
-		emoji = "🌙";
-	} else {
-		salutationText="Have enough Sleep!";
-		emoji = "✨";
-	}
+	const weekday = day.getDay();
+	const salutationText = day.getSalutation();
 
 	res.render("home", {
-		date: currentDay.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
-		datePickerDefault: dateOnly,
-		day: day,
-		emoji: emoji,
-		salutation: salutationText,
+		longDate: selectedDate.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
+		datePickerDate: dateOnly,
+		weekday: weekday,
+		emoji: salutationText.emoji,
+		salutation: salutationText.salutation,
 		todoItems: todos
 	});
 });
 
 app.post("/", (req, res) => {
-	let newTodo = new Todo({name: req.body.newToDo, date: dateOnly});
-	newTodo.save().then(promise => todos.push(promise));	
+	let newTodoItem = new Todo({name: req.body.newToDo, date: dateOnly});
+	newTodoItem.save().then(promise => todos.push(promise));	
 	res.redirect("/");
 });
 
 app.post("/mark-unmark", (req, res) => {
 	let index = req.body.checkbox;
 	// console.log(req.body);
-	
-	Todo.updateOne(
-		{name: todos[index].name}, 
-		{isChecked: !todos[index].isChecked}
-	, (err) => {
+	Todo.updateOne({name: todos[index].name}, {isChecked: !todos[index].isChecked}, (err) => {
 		if(err) {
 			console.log(err);
 		}
 	});
 	
 	todos[index].isChecked = !todos[index].isChecked;
-
 	res.redirect("/");
 });
 
-app.post("/delete-to-do", (req, res) => {
+app.post("/delete-todo", (req, res) => {
 	let todoIdToDelete = req.body.deleteTodo;
 	
 	Todo.deleteOne({_id: todoIdToDelete}, (err) => {
@@ -106,14 +70,13 @@ app.post("/delete-to-do", (req, res) => {
 	});
 
 	todos = todos.filter(todo => todo._id != todoIdToDelete);
-	
 	res.redirect("/");
 });
 
 app.post("/change-date", (req, res) => {
 	dateOnly = req.body.dateToQuery;
-	currentDay = new Date(dateOnly);
-	//console.log(currentDay.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }));
+	selectedDate = new Date(dateOnly);
+	
 	Todo.find({date: req.body.dateToQuery}, (err, foundTodos) => {
 		if(err) {
 			console.log(err);
@@ -131,6 +94,6 @@ app.listen(3000, () => {
 		} else {
 			todos = foundTodos;
 		}
-	}) 
+	});
 	console.log("Server started at port 3000");
 });
